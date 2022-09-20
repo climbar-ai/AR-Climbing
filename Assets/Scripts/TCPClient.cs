cadb34f28f4168e04e50a5bb3693492fbc601423
jdcast <john.d.cast@gmail.com>
2022-09-20 14:54:24 -0400

    @wip add more functionality to basic TCPClient

--- Assets/Scripts/TCPClient.cs
+++ Assets/Scripts/TCPClient.cs
@@ -1,13 +1,213 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
 using UnityEngine;
+
+#if !UNITY_EDITOR
+using System.Threading.Tasks;
+#endif
 
 public class TCPClient : MonoBehaviour
 {
     [SerializeField] private GameObject holdsParent;
 
-    private void SaveHolds()
+#if !UNITY_EDITOR
+    private bool _useUWP = true;
+    private Windows.Networking.Sockets.StreamSocket socket;
+    private Task exchangeTask;
+#endif
+
+#if UNITY_EDITOR
+    private bool _useUWP = false;
+    System.Net.Sockets.TcpClient client;
+    System.Net.Sockets.NetworkStream stream;
+    private Thread exchangeThread;
+#endif
+
+    private Byte[] bytes = new Byte[256];
+    private StreamWriter writer;
+    private StreamReader reader;
+
+    public void Start()
     {
-        //TODO
+        //Server ip address and port
+        Connect("10.203.94.234", "8081");
+    }
+
+    public void Connect(string host, string port)
+    {
+        if (_useUWP)
+        {
+            ConnectUWP(host, port);
+        }
+        else
+        {
+            ConnectUnity(host, port);
+        }
+    }
+
+#if UNITY_EDITOR
+    private void ConnectUWP(string host, string port)
+#else
+    private async void ConnectUWP(string host, string port)
+#endif
+    {
+#if UNITY_EDITOR
+        errorStatus = "UWP TCP client used in Unity!";
+#else
+        try
+        {
+            if (exchangeTask != null) StopExchange();
+
+            socket = new Windows.Networking.Sockets.StreamSocket();
+            Windows.Networking.HostName serverHost = new Windows.Networking.HostName(host);
+            await socket.ConnectAsync(serverHost, port);
+
+            Stream streamOut = socket.OutputStream.AsStreamForWrite();
+            writer = new StreamWriter(streamOut) { AutoFlush = true };
+
+            Stream streamIn = socket.InputStream.AsStreamForRead();
+            reader = new StreamReader(streamIn);
+
+            successStatus = "Connected!";
+        }
+        catch (Exception e)
+        {
+            errorStatus = e.ToString();
+        }
+#endif
+    }
+
+    private void ConnectUnity(string host, string port)
+    {
+#if !UNITY_EDITOR
+        errorStatus = "Unity TCP client used in UWP!";
+#else
+        try
+        {
+            if (exchangeThread != null) StopExchange();
+
+            client = new System.Net.Sockets.TcpClient(host, Int32.Parse(port));
+            stream = client.GetStream();
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream) { AutoFlush = true };
+
+            successStatus = "Connected!";
+        }
+        catch (Exception e)
+        {
+            errorStatus = e.ToString();
+        }
+#endif
+    }
+
+    private bool exchanging = false;
+    private bool exchangeStopRequested = false;
+    private string lastPacket = null;
+
+    private string errorStatus = null;
+    private string successStatus = null;
+
+    public void Update()
+    {
+        if (errorStatus != null)
+        {
+            Debug.Log(errorStatus);
+            errorStatus = null;
+        }
+        if (successStatus != null)
+        {
+            Debug.Log(successStatus);
+            successStatus = null;
+        }
+    }
+
+    public void SendFile(string filename="")
+    {
+        try
+        {
+            // notify server of endpoint to use
+            writer.Write("startAlarm");
+
+            // send filename
+            writer.Write(filename);
+
+            // send file contents
+            string path = Path.Combine(Application.persistentDataPath, filename);
+            using (TextReader fReader = File.OpenRead(path))
+            {
+                for (int i=0; i < fReader.read)
+                fReader.ReadLine();
+
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+
+    public void CloseConnection()
+    {
+        exchangeStopRequested = true;
+
+#if UNITY_EDITOR
+        if (exchangeThread != null)
+        {
+            exchangeThread.Abort();
+            stream.Close();
+            client.Close();
+            writer.Close();
+            reader.Close();
+
+            stream = null;
+            exchangeThread = null;
+        }
+#else
+        if (exchangeTask != null)
+        {
+            exchangeTask.Wait();
+            socket.Dispose();
+            writer.Dispose();
+            reader.Dispose();
+
+            socket = null;
+            exchangeTask = null;
+        }
+#endif
+        writer = null;
+        reader = null;
+    }
+
+    public void OnDestroy()
+    {
+        CloseConnection();
+    }
+
+    public void SaveHolds()
+    {
+        string filename = "holds.txt";
+        CreateFile(filename: filename);
+
+        // send data
+        SendFile(filename: filename);
+
+        // close connection so we can later reconnect
+        CloseConnection();
+
+        //DeleteFile();
+    }
+
+    private void CreateFile(string filename="")
+    {
+        string path = Path.Combine(Application.persistentDataPath, filename);
+        using (TextWriter fWriter = File.CreateText(path))
+        {
+            fWriter.WriteLine($"Time: {Time.time}");
+            fWriter.WriteLine("Hello World");
+        }
     }
 }
