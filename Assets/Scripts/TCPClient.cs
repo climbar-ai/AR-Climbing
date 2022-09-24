a9f97fdc0e3f53abf5274ca5eba753fd0f76b50d
jdcast <john.d.cast@gmail.com>
2022-09-23 20:40:03 -0400

    Found/fix bug in server that was closing port -> cleaned up code now to use constantly open port instead of opening/closing on each call; Removed unecessary canvasRenderer from elements in scene to get rid of build warnings

--- Assets/Scripts/TCPClient.cs
+++ Assets/Scripts/TCPClient.cs
@@ -1,3 +1,9 @@
+/// TCPClient for HL2
+/// 1. https://medium.datadriveninvestor.com/connecting-the-microsoft-hololens-and-raspberry-pi3b-58665032964c
+/// 2. https://stackoverflow.com/questions/46964007/python-socket-connection-between-raspberry-pi-s
+/// 3. https://stackoverflow.com/questions/56194446/send-big-file-over-socket
+/// 4. https://nikhilroxtomar.medium.com/file-transfer-using-tcp-socket-in-python3-idiot-developer-c5cf3899819c
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -5,10 +11,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
-
-#if !UNITY_EDITOR
-using System.Threading.Tasks;
-#endif
 
 
 public class TCPClient : MonoBehaviour
@@ -20,99 +22,88 @@ public class TCPClient : MonoBehaviour
 
 #if !UNITY_EDITOR
     private bool _useUWP = true;
-    //private Windows.Networking.Sockets.StreamSocket socket;
-    private Task exchangeTask;
+    private Windows.Networking.Sockets.StreamSocket socket;
 #endif
 
 #if UNITY_EDITOR
     private bool _useUWP = false;
-    //System.Net.Sockets.TcpClient client;
-    //System.Net.Sockets.NetworkStream stream;
-    private Thread exchangeThread;
+    System.Net.Sockets.TcpClient client;
+    System.Net.Sockets.NetworkStream stream;
 #endif
 
-    //private Byte[] bytes = new Byte[256];
     private StreamWriter writer;
     private StreamReader reader;
 
     private int BUFFER_SIZE = 1024;
 
-    //public void Start()
-    //{
-    //    //Server ip address and port
-    //    Connect("10.203.94.234", "8081");
-    //}
+    public void Start()
+    {
+        //Server ip address and port
+        Connect(host, port);
+    }
 
-    //public void Connect(string host, string port)
-    //{
-    //    if (_useUWP)
-    //    {
-    //        ConnectUWP(host, port);
-    //    }
-    //    else
-    //    {
-    //        ConnectUnity(host, port);
-    //    }
-    //}
+    public void Connect(string host, string port)
+    {
+        if (_useUWP)
+        {
+            ConnectUWP(host, port);
+        }
+        else
+        {
+            ConnectUnity(host, port);
+        }
+    }
 
-//#if UNITY_EDITOR
-//    private void ConnectUWP(string host, string port)
-//#else
-//    private async void ConnectUWP(string host, string port)
-//#endif
-//    {
-//#if UNITY_EDITOR
-//        errorStatus = "UWP TCP client used in Unity!";
-//#else
-//        try
-//        {
-//            if (exchangeTask != null) CloseConnection();
+#if UNITY_EDITOR
+        private void ConnectUWP(string host, string port)
+#else
+    private async void ConnectUWP(string host, string port)
+#endif
+    {
+#if UNITY_EDITOR
+            errorStatus = "UWP TCP client used in Unity!";
+#else
+        try
+        {
+            socket = new Windows.Networking.Sockets.StreamSocket();
+            Windows.Networking.HostName serverHost = new Windows.Networking.HostName(host);
+            await socket.ConnectAsync(serverHost, port);
 
-//            socket = new Windows.Networking.Sockets.StreamSocket();
-//            Windows.Networking.HostName serverHost = new Windows.Networking.HostName(host);
-//            await socket.ConnectAsync(serverHost, port);
+            Stream streamOut = socket.OutputStream.AsStreamForWrite();
+            writer = new StreamWriter(streamOut) { AutoFlush = true };
 
-//            Stream streamOut = socket.OutputStream.AsStreamForWrite();
-//            writer = new StreamWriter(streamOut) { AutoFlush = true };
+            Stream streamIn = socket.InputStream.AsStreamForRead();
+            reader = new StreamReader(streamIn);
 
-//            Stream streamIn = socket.InputStream.AsStreamForRead();
-//            reader = new StreamReader(streamIn);
+            successStatus = "Connected!";
+        }
+        catch (Exception e)
+        {
+            errorStatus = e.ToString();
+        }
+#endif
+    }
 
-//            successStatus = "Connected!";
-//        }
-//        catch (Exception e)
-//        {
-//            errorStatus = e.ToString();
-//        }
-//#endif
-//    }
+    private void ConnectUnity(string host, string port)
+    {
+#if !UNITY_EDITOR
+        errorStatus = "Unity TCP client used in UWP!";
+#else
+            try
+            {
+                client = new System.Net.Sockets.TcpClient(host, Int32.Parse(port));
+                stream = client.GetStream();
+                reader = new StreamReader(stream);
+                writer = new StreamWriter(stream) { AutoFlush = true };
 
-//    private void ConnectUnity(string host, string port)
-//    {
-//#if !UNITY_EDITOR
-//        errorStatus = "Unity TCP client used in UWP!";
-//#else
-//        try
-//        {
-//            if (exchangeThread != null) CloseConnection();
-
-//            client = new System.Net.Sockets.TcpClient(host, Int32.Parse(port));
-//            stream = client.GetStream();
-//            reader = new StreamReader(stream);
-//            writer = new StreamWriter(stream) { AutoFlush = true };
-
-//            successStatus = "Connected!";
-//        }
-//        catch (Exception e)
-//        {
-//            errorStatus = e.ToString();
-//        }
-//#endif
-//    }
-
-    private bool exchanging = false;
-    private bool exchangeStopRequested = false;
-    private string lastPacket = null;
+                successStatus = "Connected!";
+            }
+            catch (Exception e)
+            {
+                errorStatus = e.ToString();
+            }
+#endif
+    }
 
     private string errorStatus = null;
     private string successStatus = null;
@@ -141,133 +132,51 @@ public class TCPClient : MonoBehaviour
     /// <param name="filename"></param>
     /// <returns></returns>
 
-    public async Task SendFileUnity(string filename = "")
+    public async Task SendFile(string filename = "")
     {
-#if !UNITY_EDITOR
-        errorStatus = "Unity TCP client used in UWP!";
-#else
-        // connect to server
-        using (System.Net.Sockets.TcpClient client = new System.Net.Sockets.TcpClient(host, Int32.Parse(port)))
-        using (System.Net.Sockets.NetworkStream stream = client.GetStream())
-        using (StreamReader reader = new StreamReader(stream))
-        using (StreamWriter writer = new StreamWriter(stream) { AutoFlush = true })
+        try
         {
-            try
+            Debug.Log($"filename: {filename}");
+
+            char[] response = new char[BUFFER_SIZE];
+
+            // notify server of endpoint to use
+            writer.Write("receiveFile");
+            Debug.Log("written");
+
+            // get receipt confirmation
+            await reader.ReadAsync(response, 0, BUFFER_SIZE);
+            string responseStr = new string(response);
+            responseStr = responseStr.Trim(new Char[] { '\0' }); // trim any empty bytes in the buffer
+            if (responseStr.Length <= 0 || !responseStr.Equals("ready")) { return; }
+
+            Debug.Log($"response: {responseStr}");
+            Debug.Log(writer);
+            Debug.Log("sending filename...");
+
+            // send filename
+            writer.Write(filename);
+
+            // get receipt confirmation
+            await reader.ReadAsync(response, 0, BUFFER_SIZE);
+            responseStr = new string(response);
+            responseStr = responseStr.Trim(new Char[] { '\0' }); // trim any empty bytes in the buffer
+            if (responseStr.Length <= 0 || !responseStr.Equals("ready")) { return; }
+
+            Debug.Log($"response: {responseStr}");
+
+            Debug.Log("sending file contents...");
+
+            // send file contents
+            string path = Path.Combine(Application.persistentDataPath, filename);
+            using (TextReader sr = File.OpenText(path))
             {
-                Debug.Log($"filename: {filename}");
-
-                char[] response = new char[BUFFER_SIZE];
-
-                // notify server of endpoint to use
-                writer.Write("receiveFile");
-                Debug.Log("written");
-
-                // get receipt confirmation
-                await reader.ReadAsync(response, 0, BUFFER_SIZE);
-                string responseStr = new string(response);
-                responseStr = responseStr.Trim(new Char[] { '\0' }); // trim any empty bytes in the buffer
-                if (responseStr.Length <= 0 || !responseStr.Equals("ready")) { return; }
-
-                Debug.Log($"response: {responseStr}");
-                Debug.Log(writer);
-                Debug.Log("sending filename...");
-
-                // send filename
-                writer.Write(filename);
-
-                // get receipt confirmation
-                await reader.ReadAsync(response, 0, BUFFER_SIZE);
-                responseStr = new string(response);
-                responseStr = responseStr.Trim(new Char[] { '\0' }); // trim any empty bytes in the buffer
-                if (responseStr.Length <= 0 || !responseStr.Equals("ready")) { return; }
-
-                Debug.Log($"response: {responseStr}");
-
-                Debug.Log("sending file contents...");
-
-                // send file contents
-                string path = Path.Combine(Application.persistentDataPath, filename);
-                using (TextReader sr = File.OpenText(path))
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
                 {
-                    string s = "";
-                    while ((s = sr.ReadLine()) != null)
-                    {
-                        // send line of file to server
-                        Debug.Log($"Tx: {s}");
-                        writer.Write(s + "\n");
-
-                        // get receipt confirmation
-                        await reader.ReadAsync(response, 0, BUFFER_SIZE);
-                        responseStr = new string(response);
-                        responseStr = responseStr.Trim(new Char[] { '\0' }); // trim any empty bytes in the buffer
-                        if (responseStr.Length <= 0 || !responseStr.Equals("ready")) { return; }
-
-                        Debug.Log($"response: {responseStr}");
-                    }
-                    Debug.Log("Tx: Finished");
-                    writer.Write("done");
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.Log(e.ToString());
-            }
-        }
-#endif
-    }
-
-    /// <summary>
-    /// Sends a file to server
-    /// NOTE: must be async because we are using threading: https://stackoverflow.com/questions/45717656/losing-variable-after-async-call.  
-    /// Once a synchronous function sees an await, it will turn over control and when it resumes,
-    /// a new thread with a new context will take over after the await.  Thus, object references will be null after the await if using a synchronous call.
-    /// NOTE: the reader must be async as otherwise it hangs the main thread in unity and the app exits when trying to perform Read()
-    /// </summary>
-    /// <param name="filename"></param>
-    /// <returns></returns>
-
-#if UNITY_EDITOR
-    private async void SendFileUWP(string filename = "")
-#else
-    public async Task SendFileUWP(string filename = "")
-#endif
-    {
-#if UNITY_EDITOR
-        errorStatus = "Unity TCP client used in UWP!";
-#else
-        // connect to server
-        using (Windows.Networking.Sockets.StreamSocket socket = new Windows.Networking.Sockets.StreamSocket()) 
-        {
-            Windows.Networking.HostName serverHost = new Windows.Networking.HostName(host);
-            await socket.ConnectAsync(serverHost, port);
-
-            using (Stream streamOut = socket.OutputStream.AsStreamForWrite())
-            using (StreamWriter writer = new StreamWriter(streamOut) { AutoFlush = true })
-            using (Stream streamIn = socket.InputStream.AsStreamForRead())
-            using (StreamReader reader = new StreamReader(streamIn))
-            {
-                try
-                {
-                    Debug.Log($"filename: {filename}");
-
-                    char[] response = new char[BUFFER_SIZE];
-
-                    // notify server of endpoint to use
-                    writer.Write("receiveFile");
-                    Debug.Log("written");
-
-                    // get receipt confirmation
-                    await reader.ReadAsync(response, 0, BUFFER_SIZE);
-                    string responseStr = new string(response);
-                    responseStr = responseStr.Trim(new Char[] { '\0' }); // trim any empty bytes in the buffer
-                    if (responseStr.Length <= 0 || !responseStr.Equals("ready")) { return; }
-
-                    Debug.Log($"response: {responseStr}");
-                    Debug.Log(writer);
-                    Debug.Log("sending filename...");
-
-                    // send filename
-                    writer.Write(filename);
+                    // send line of file to server
+                    Debug.Log($"Tx: {s}");
+                    writer.Write(s + "\n");
 
                     // get receipt confirmation
                     await reader.ReadAsync(response, 0, BUFFER_SIZE);
@@ -276,77 +185,66 @@ public class TCPClient : MonoBehaviour
                     if (responseStr.Length <= 0 || !responseStr.Equals("ready")) { return; }
 
                     Debug.Log($"response: {responseStr}");
-
-                    Debug.Log("sending file contents...");
-
-                    // send file contents
-                    string path = Path.Combine(Application.persistentDataPath, filename);
-                    using (TextReader sr = File.OpenText(path))
-                    {
-                        string s = "";
-                        while ((s = sr.ReadLine()) != null)
-                        {
-                            // send line of file to server
-                            Debug.Log($"Tx: {s}");
-                            writer.Write(s + "\n");
-
-                            // get receipt confirmation
-                            await reader.ReadAsync(response, 0, BUFFER_SIZE);
-                            responseStr = new string(response);
-                            responseStr = responseStr.Trim(new Char[] { '\0' }); // trim any empty bytes in the buffer
-                            if (responseStr.Length <= 0 || !responseStr.Equals("ready")) { return; }
-
-                            Debug.Log($"response: {responseStr}");
-                        }
-                        Debug.Log("Tx: Finished");
-                        writer.Write("done");
-                    }
                 }
-                catch (Exception e)
-                {
-                    Debug.Log(e.ToString());
-                }
+                    
+                // signal done to server
+                Debug.Log("Tx: Finished");
+                writer.Write("done");
             }
         }
-#endif
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+        }
     }
 
-//    public void CloseConnection()
-//    {
-//        exchangeStopRequested = true;
+    public void CloseConnection()
+    {
+#if UNITY_EDITOR
+        stream.Close();
+        client.Close();
+        writer.Close();
+        reader.Close();
 
-//#if UNITY_EDITOR
-//        if (exchangeThread != null)
-//        {
-//            exchangeThread.Abort();
-//            stream.Close();
-//            client.Close();
-//            writer.Close();
-//            reader.Close();
+        stream = null;
+#else
+        socket.Dispose();
+        writer.Dispose();
+        reader.Dispose();
 
-//            stream = null;
-//            exchangeThread = null;
-//        }
-//#else
-//        if (exchangeTask != null)
-//        {
-//            exchangeTask.Wait();
-//            socket.Dispose();
-//            writer.Dispose();
-//            reader.Dispose();
+        socket = null;
+#endif
+        writer = null;
+        reader = null;
+    }
 
-//            socket = null;
-//            exchangeTask = null;
-//        }
-//#endif
-//        writer = null;
-//        reader = null;
-//    }
 
-    //public void OnDestroy()
-    //{
-    //    CloseConnection();
-    //}
+
+    /// <summary>
+    /// Perform cleanup
+    /// NOTE: OnApplicationFocus used because OnDestroy are not reliabe on HL2 headset and also because OnApplicatoinQuit and OnDisable don't appear to
+    /// be called when the app is exited by user on HL2.  Also, not clear if this function is called on app crashes.  However, OnApplicationFocus gets
+    /// called if click out of play screen in Unity Play mode, which is annoying.  So we use OnDestroy when running on Unity Play mode.
+    /// https://stackoverflow.com/questions/28647118/detect-application-quit-with-unity
+    /// </summary>
+#if UNITY_EDITOR
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+        writer.Write("closeSock");
+        CloseConnection();
+    }
+#else
+    void OnApplicationFocus(bool focus)
+    {
+        if (!focus)
+        {
+            Debug.Log("OnApplicationFocus");
+            writer.Write("closeSock");
+            CloseConnection();
+        }
+    }
+#endif
 
     /// <summary>
     /// Registered in Inspector.
@@ -358,11 +256,7 @@ public class TCPClient : MonoBehaviour
         CreateFile(filename: filename);
 
         // send data
-#if UNITY_EDITOR
-        await SendFileUnity(filename: filename);
-#else 
-        await SendFileUWP(filename: filename);
-#endif
+        await SendFile(filename: filename);
 
         // remove the file so we don't accrue files
         DeleteFile(filename: filename);
